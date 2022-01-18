6e6295b02f2dd752cff5b591388d986e73fb7cec
Sandro Rüfenacht <sandro.ruefenacht0@gmail.com>
2022-01-19 00:33:15 +0100

    add solution for day 4 part 2

--- day_04/Bingo.js
+++ day_04/Bingo.js
@@ -16,6 +16,31 @@ class Bingo {
         return false;
     }
 
+    readAllNumbers(numbers) {
+        let cardCache = false;
+        let numberCache = -1;
+        let sumCache = false;
+
+        let foundIndexes = [];
+
+        for(let number of numbers) {
+            for(let i = 0; i < this.cards.length; i++) {
+                if(!foundIndexes.includes(i)) {
+                    this.cards[i].findNumber(number);
+
+                    if(this.cards[i].wins()) {
+                        cardCache = this.cards[i];
+                        numberCache = number;
+                        sumCache = this.cards[i].getSumOfUnmarked();
+
+                        foundIndexes.push(i);
+                    }
+                }
+            }
+        }
+        return {cardCache, numberCache, sumCache};
+    }
+
     getWinner() {
         for(let card of this.cards) {
             if(card.wins()) {
--- day_04/part_2/main.js
+++ day_04/part_2/main.js
@@ -1,6 +1,26 @@
+const fs = require('fs');
+const { BingoCard } = require('../BingoCard');
+const { Bingo } = require('../Bingo');
 
 function partTwo() {
+    const jsonData = fs.readFileSync('day_04/input.json', 'utf8');
+    const data = JSON.parse(jsonData);
 
+    let cardCollection = [];
+    for(let card of data.cards) {
+        cardCollection.push(new BingoCard(card));
+    }
+
+    let bingo = new Bingo(cardCollection);
+    let result = bingo.readAllNumbers(data.numbers);
+    
+    if(result.cardCache !== false) {
+        let sum = result.cardCache.getSumOfUnmarked();
+
+        console.log("Result: " + (result.sumCache * parseInt(result.numberCache)));
+    }else{
+        console.log("No winner found :(");
+    }
 }
 
 module.exports.partTwo = partTwo;
